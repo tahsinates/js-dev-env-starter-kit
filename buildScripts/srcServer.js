fae519cdc7417e39e59a08927f9baddabf4d7010
Tahsin Ates <>
2018-10-18 17:58:02 +0100

    added ESLint and for Unit tests added Mocha & Chai

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 
+/* eslint-disable no-console */
+
 //For WebPack configuration
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
