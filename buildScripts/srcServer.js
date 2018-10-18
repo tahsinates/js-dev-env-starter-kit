70a682185a6262418b0df82832b60f3b7b899956
Tahsin Ates <>
2018-10-18 20:34:36 +0100

    added mock data configuration with JSON Schema Faker and JSON Server

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -22,6 +22,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users', function(req,res) {
+  // Hard codding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id":1, "firstName":"Tahsin", "lastName":"Ates", "email":"ttttaaaa@gmail.com"},
+    {"id":2, "firstName":"Huseyin", "lastName":"Genc", "email":"hhhhgggg@gmail.com"},
+    {"id":3, "firstName":"Burcu", "lastName":"Babel", "email":"bbbbeeee@gmail.com"}
+  ]);
+});
+
 app.listen(port,function(err){
   if(err){
     console.log(err);
